2899ecafde28fa001e1b2abb225bbc8ce4ed8777
PrathyushaAmbati34 <prathyushaambati34@gmail.com>
2026-05-22 23:29:59 +0530

    Update Movie.java

--- src/main/java/com/example/demo/entity/Movie.java
+++ src/main/java/com/example/demo/entity/Movie.java
@@ -4,16 +4,49 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
-import lombok.Data;
-
 @Entity
-@Data
 public class Movie {
-	@Id
-	private int id;
-	@NotEmpty
-	private String movieName;
-    private String movieGenere;
-   
 
+    @Id
+    private Integer id;
+
+    @NotEmpty(message = "Movie name cannot be empty")
+    private String movieName;
+
+    @NotEmpty(message = "Genre cannot be empty")
+    private String movieGenre;
+
+    public Movie() {
+        // default constructor
+    }
+
+    public Movie(Integer id, String movieName, String movieGenre) {
+        this.id = id;
+        this.movieName = movieName;
+        this.movieGenre = movieGenre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieGenre() {
+        return movieGenre;
+    }
+
+    public void setMovieGenre(String movieGenre) {
+        this.movieGenre = movieGenre;
+    }
 }
